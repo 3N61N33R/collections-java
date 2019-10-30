2248cb45dc521c65b6439bb2195bd5e550629bd9
graycadeau <g.zawadi.w@gmail.com>
2019-10-30 16:18:12 +0300

    creates basic form of arraylist

---
+++ collections/Arraylist.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arraylist {
+
+    public static void main(String[] args) {
+
+        List<String> companies = new ArrayList<>();
+
+        companies.add("Google");
+
+        System.out.println(companies);
+
+    }
+
+}
